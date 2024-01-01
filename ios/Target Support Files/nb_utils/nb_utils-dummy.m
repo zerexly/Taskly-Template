86a6445d96540fd0543e04d18e094a13985e93f8
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-01-01 16:22:44 +0530

    Delete ios directory

--- ios/Target Support Files/nb_utils/nb_utils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_nb_utils : NSObject
-@end
-@implementation PodsDummy_nb_utils
-@end
--- ios/Target Support Files/smart_auth/smart_auth-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_smart_auth : NSObject
-@end
-@implementation PodsDummy_smart_auth
-@end
