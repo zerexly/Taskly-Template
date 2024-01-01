6450c21a6cbb0b5acd856cae6156990c44d5a519
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-01-01 16:33:54 +0530

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nikhilroy/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nikhilroy/Documents/Brixe Technology/Flutter UI/Task Design/task_theme"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
